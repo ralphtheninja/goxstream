49ba4c5145ce659448bb84d17fd636f0c668cc94
Lars-Magnus Skog <lars.magnus.skog@gmail.com>
2013-05-08 17:58:04 +0200

    apparently not working for 0.9.3 because no Transform, fixed

--- readable.js
+++ readable.js
@@ -1,7 +1,7 @@
-if (process.version.match(/v([0-9]+\.[0-9]+)\.[0-9]+/)[1] == '0.8') {
+var version = process.version.match(/v([0-9]+\.[0-9]+)\.[0-9]+/)[1]
+if (version == '0.8' || version == '0.9') {
   module.exports = require('readable-stream');
 }
 else {
   module.exports = require('stream').Readable
 }
-
